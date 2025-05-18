90a309a99923addb88aabd7f33f68053d1811b9f
Ayush Tomar <ayushsinghtomar184@gmail.com>
2025-05-18 20:36:33 +0530

    update the code

--- adaptive-learning/vite.config.ts
+++ adaptive-learning/vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EDTechPlatForm/',
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-}); 
+  base: './', 
+})
